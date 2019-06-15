7ee0c91ae9e0b8aa067ebc84d0c2e111812326de
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-06-15 15:11:17 -0400

    polishes nav bar & links on mobile

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-let btn = document.getElementById('menu-btn');
+let list = document.getElementsByClassName('container');
 let navLi = document.getElementById('nav-li');
-btn.addEventListener('click', function() {
+let menu = list[0];
+
+menu.addEventListener('click', function() {
   if (navLi.style.display === 'block') {
     navLi.style.display = 'none';
+    menu.classList.toggle('change');
   } else {
     navLi.style.display = 'block';
+    menu.classList.toggle('change');
   }
 });
+
+let navLinks = document.getElementsByClassName('nav-links');
+for (let link of navLinks) {
+  link.addEventListener('click', function() {
+    let width = Window.innerWidth;
+    if (width <= 600) {
+      menu.classList.remove('change');
+      navLi.style.display = 'none';
+    }
+  })
+}
