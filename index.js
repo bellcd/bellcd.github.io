17c4cd0768f9cff17245dd959b86f30a6ca8527a
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2019-06-17 14:27:03 -0400

    refactors nav menu

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 let list = document.getElementsByClassName('container');
 let navLi = document.getElementById('nav-li');
 let menu = list[0];
+let nav = document.getElementsByTagName('nav')[0];
+let menuUl = document.getElementById('menu-ul');
 
 menu.addEventListener('click', function() {
+  // console.log('hello!');
+  console.log(nav);
   if (navLi.style.display === 'block') {
     navLi.style.display = 'none';
+    menuUl.classList.toggle('full-screen');
     menu.classList.toggle('change');
   } else {
     navLi.style.display = 'block';
+    menuUl.classList.toggle('full-screen');
     menu.classList.toggle('change');
   }
 });
@@ -17,7 +23,18 @@ for (let link of navLinks) {
   link.addEventListener('click', function() {
     if (window.innerWidth <= 600) {
       menu.classList.remove('change');
+      menuUl.classList.toggle('full-screen');
       navLi.style.display = 'none';
     }
   })
+}
+
+if (window.innerWidth >= 600) {
+  let header = document.getElementById('header-ul');
+  let sidebarHeader = header.cloneNode(true);
+  let nav = document.getElementsByTagName('nav')[0];
+
+
+  nav.insertBefore(sidebarHeader, menuUl);
+  document.getElementById('details').style.display = 'none';
 }
